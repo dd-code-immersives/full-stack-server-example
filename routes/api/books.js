a926f6eacc62407f213e81a384daa91c60d0a6ef
dd-code-immersives <domilamad89@gmail.com>
2023-03-14 11:58:19 -0400

    implemented update and delete routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -5,7 +5,6 @@ const router = express.Router();
 //load book model 
 const Book = require('../../models/Book')
 
-
 // test route, to make sure API routing is running 
 router.get('/test', (req,res) => res.send("book route testing"))
 
@@ -30,6 +29,23 @@ router.post('/', (req, res) => {
     Book.create(req.body)
         .then(book => res.json({msg: 'Book added successfully'}))
         .catch(err => res.status(400).json({error: err}))
+})
+
+
+// delete a new book 
+router.delete('/:id', (req, res) => {
+    Book.findByIdAndDelete(req.params.id, req.body)
+    .then(book => res.json({msg: 'Book entry deleted successfully'}))
+    .catch(err => res.status(404).json({error: 'No such book'}))
+})
+
+// update a new book
+router.put('/:id', (req, res) => {
+    Book.findByIdAndUpdate(req.params.id, req.body)
+    .then(book => res.json({ msg: 'updated successfully'}))
+    .catch(err => 
+        res.status(400).json({error: 'Unable to update the Database'})
+    );
 })
 
 module.exports = router;
